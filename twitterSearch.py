bb035764497df71f62a8a6d527f3a8698f3807d9
Andy Sayler <andy.sayler@gmail.com>
2013-04-14 20:42:19 -0600

    Added more to count function

--- twitterSearch.py
+++ twitterSearch.py
@@ -1,14 +1,33 @@
+import sys
 import urllib.request as request
 import urllib.parse as parse
+import json
 
-def searchTweets(query):
+#Uses Version 1.0 of the Twitter Search API: https://dev.twitter.com/docs/api/1/get/search
+APIURL     = "http://search.twitter.com/search.json"
+MAXQLENGTH = 1000     # Max 1000 for Twitter Search API
+RCOUNT     = 100      # Max 100 for Twitter Search API
 
-    print("Searching for: " + query)
-    encQuery = parse.quote(query)
-    print("Searching for (encoded): " + encQuery)
-    res = request.urlopen("http://search.twitter.com/search.json?q="+encQuery)
-    resText = res.read()
-    print("Result:\n" + resText.decode('utf-8'))
+ENCODING = 'utf-8'
+RESULTSKEY = 'results'
 
-# we will search tweets about "fc liverpool" football team
-searchTweets("#CU_CSFP_RPi")
+EXIT_FAILURE = -1
+
+def countTweets(query):
+
+    queryEnc = parse.quote(query)
+    if(len(queryEnc) > MAXQLENGTH):
+        sys.stderr.write("Query Too Long")
+        return EXIT_FAILURE
+    url = APIURL + "?q=" + queryEnc + "&rpp=" + str(RCOUNT)
+    res = request.urlopen(url)
+    resData = res.read()
+    resStr = resData.decode(ENCODING)
+    resDec = json.loads(resStr)
+    return len(resDec[RESULTSKEY])
+
+query = "@EmWatson" #"#CU_CSFP_RPi"
+cnt = countTweets(query)
+print("There are " + str(cnt) + " Tweets that match " + query)
+
+
