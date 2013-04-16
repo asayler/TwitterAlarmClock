fbb996cb8845695d9401a046466ac85bd8b38a2e
Andy Sayler <andy.sayler@gmail.com>
2013-04-15 22:04:48 -0600

    Added comments and removed LED reference

--- twitterAlarm.py
+++ twitterAlarm.py
@@ -1,26 +1,39 @@
+#!/usr/bin/python3
+#
+# twitterAlarm.py
+#
+# By Andy Sayler (www.andysayler.com)
+# April 2013
+#
+# A basic program for counting tweets matching the provided search
+# query and triggering the playing of an alarm.wav file when the count
+# exceeds the provided threshold
+#
+# Note: Has some limits (max count of 100, no auth, etc)
+
+# Std Library Imports
 import sys
 import argparse
 import time
 import subprocess
-
 import urllib.request as request
 import urllib.parse as parse
 import json
 
+# Constants
 TIMEOUT = 10 # in seconds
-
-#Uses Version 1.0 of the Twitter Search API: https://dev.twitter.com/docs/api/1/get/search
+EXIT_FAILURE = -1
+EXIT_SUCCESS = 0
+# Uses Version 1.0 of the Twitter Search API:
+# https://dev.twitter.com/docs/api/1/get/search
 APIURL     = "http://search.twitter.com/search.json"
-MAXQLENGTH = 1000     # Max 1000 for Twitter Search API
-RCOUNT     = 100      # Max 100 for Twitter Search API
-
+MAXQLENGTH = 1000      # Max 1000 for Twitter Search API
+RCOUNT     = 100       # Max 100 for Twitter Search API
 ENCODING = 'utf-8'
 RESULTSKEY = 'results'
 
-EXIT_FAILURE = -1
-
+# A function to query the search API and count the results
 def countTweets(query):
-
     queryEnc = parse.quote(query)
     if(len(queryEnc) > MAXQLENGTH):
         sys.stderr.write("Query Too Long")
@@ -32,16 +45,19 @@ def countTweets(query):
     resDec = json.loads(resStr)
     return len(resDec[RESULTSKEY])
 
+# Setup Argument Parsing
 parser = argparse.ArgumentParser(description='Sound alarm when N tweets are detected')
 parser.add_argument('query', type=str,
                    help='Twitter search query')
 parser.add_argument('count', type=int,
                    help='Tweet threshold to sound alarm (max 99)')
 
+# Parse Arguments
 args = parser.parse_args()
 query = args.query
 threshold = args.count
 
+# Loop until threshold is reached
 cnt = countTweets(query)
 while(cnt < threshold):
     time.sleep(TIMEOUT)
@@ -52,12 +68,13 @@ while(cnt < threshold):
     sys.stdout.write("There are " + str(cnt) + " Tweets that match " + query + "\n")
     sys.stdout.write("Alarm will" + (" not " if (cnt < threshold) else " ") + "sound\n")
 
-led = 0
-
+# Sound alarm if threshold has been reached
 if(cnt >= threshold):
     p = subprocess.Popen(["aplay", "alarm.wav"])
     while(p.poll() == None):
-        led = (led + 1) % 8
         time.sleep(1)
-
-sys.exit(0)
+    # Exit on a happy note
+    sys.exit(EXIT_SUCCESS)
+else:
+    # Or not...
+    sys.exit(EXIT_FAILURE)
